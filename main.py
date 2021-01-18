e39f36ec2d6111fa18e1bc021f85aa6e8ae8bf63
Spencer Lepine <spencerlepine26@gmail.com>
2021-01-18 12:20:35 -0500

    add main.py

---
+++ main.py
@@ -0,0 +1,104 @@
+# @SpencerLepine - Connect Four command-line game made w/ Python
+
+# To do:
+# piece limit / board is full?
+
+# Hard-coded board size
+BOARD_COLS = 7
+BOARD_ROWS = 6
+
+# Game board object
+class Board():
+    def __init__(self):
+        self.board = [[' ' for _ in range(BOARD_COLS)] for _ in range(BOARD_ROWS)]
+        self.turns = 0
+        self.last_move = [-1, -1] # [r, c]
+
+    def print_board(self):
+        print("\n")
+        # Number the columns seperately to keep it cleaner
+        for r in range(BOARD_COLS):
+            print(f"  ({r+1}) ", end="")
+        print("\n")
+
+        # Print the slots of the game board
+        for r in range(BOARD_ROWS):
+            print('|', end="")
+            for c in range(BOARD_COLS):
+                print(f"  {self.board[r][c]}  |", end="")
+            print("\n")
+
+        print(f"{'-' * 42}\n")
+
+    def which_turn(self):
+        players = ['X', 'O']
+        return players[self.turns % 2]
+    
+    def in_array(self, r, c):
+        return (r >= 0 and r < BOARD_ROWS and c >= 0 and c < BOARD_COLS)
+
+    def turn(self, column):
+        # Search bottom up for an open slot
+        for i in range(BOARD_ROWS-1, -1, -1):
+            if self.board[i][column] == ' ':
+                self.board[i][column] = self.which_turn()
+                self.last_move = [i, column]
+
+                self.turns += 1
+                return True
+
+        return False
+
+    def check_winner(self):
+        last_row = self.last_move[0]
+        last_col = self.last_move[1]
+        last_letter = self.board[last_row][last_col]
+
+        directions = [[[-1, 0], 0, True], # Direction, letter count, locked bool
+                      [[1, 0], 0, True], 
+                      [[0, -1], 0, True],
+                      [[0, 1], 0, True],
+                      [[-1, -1], 0, True],
+                      [[1, 1], 0, True]]
+        
+        for a in range(4):
+            for d in directions:
+                r = last_row + (d[0][0] * (a+1))
+                c = last_col + (d[0][1] * (a+1))
+
+                if d[2] and self.in_array(r, c) and self.board[r][c] == last_letter:
+                    d[1] += 1
+                else:
+                    d[2] = False # Stop searching in this direction
+
+        for i in range(0, 5, 2):
+            print(f"This combo: {directions[i][1]}, {directions[i+1][1]}")
+            if (directions[i][1] + directions[i+1][1] >= 3):
+                self.print_board()
+                print(f"{last_letter} is the winner!")
+                return last_letter    
+
+        return False
+
+def play():
+    game = Board()
+
+    game_over = False
+    while not game_over:
+        game.print_board()
+
+        # Only accept valid turns
+        valid_move = False
+        while not valid_move:
+            user_move = input(f"{game.which_turn()}'s Turn - pick a column (1-{BOARD_COLS}): ")
+            try:
+                valid_move = game.turn(int(user_move)-1)
+            except:
+                print(f"Please choose a number between 1 and {BOARD_COLS}")
+
+        # End the game before the next turn
+        game_over = game.check_winner()
+
+
+if __name__ == '__main__':
+    play()
